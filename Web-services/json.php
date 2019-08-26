bcff5eb28e292c0e3ff920b0ad15ef011655d758
phil <pitso@live.co.uk>
2019-08-26 13:19:43 +0100

    added json file

---
+++ Web-services/json.php
@@ -0,0 +1,18 @@
+{"first_name": "Dale",
+"last_name": "Cooper",
+"age": 43,
+"enrolled": true,
+"married": null,
+"address": {
+"street": 123 Main street,
+"County": "Leicester",
+},
+"phone": [
+{"type": "home",
+"number": "01162 656423"	
+},
+{"type": "mobile",
+"number": "0792536152"	
+}
+]}
+
